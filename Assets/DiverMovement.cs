4d67651efe81bcfed1a3158083576a6e631c704b
winceh7 <j.pablo.suar@gmail.com>
2025-01-25 20:43:15 -0600

    fix movement with rigidbody, level creation

--- Assets/DiverMovement.cs
+++ Assets/DiverMovement.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
+
 public class UnderwaterMovement : MonoBehaviour
 {
     private Animator animator;
+    private Rigidbody2D rb2d;
 
-    public float speed = 0.001f; // Movement speed
-
-    private CharacterController controller;
-
+    public float speed = 2f; // Movement speed
     private float animationSpeedFactor = 0.3f;
 
     private float prevXNonZero = 0;
@@ -17,25 +14,28 @@ public class UnderwaterMovement : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
-        controller = GetComponent<CharacterController>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     public void SetSpeedAndDirection(float prevXNonZero, float x, float y, float speed)
     {
         float horizontalFlip = 0;
         float rotation = 0;
-        if (Math.Abs(x) < Math.Abs(y)-0.05)
-        { 
+
+        // Determine rotation based on movement direction
+        if (Math.Abs(x) < Math.Abs(y) - 0.05f)
+        {
             rotation = y < 0 ? -90 : 90;
         }
-        if (prevXNonZero < 0)
+        else if (prevXNonZero < 0)
         {
-            horizontalFlip = 180;
+            horizontalFlip = 180; // Flip horizontally if moving left
         }
 
         transform.rotation = Quaternion.Euler(0, horizontalFlip, rotation);
 
-        animator.speed = speed == 0? 1:speed * animationSpeedFactor;
+        // Update animator parameters
+        animator.speed = speed == 0 ? 1 : speed * animationSpeedFactor;
         animator.SetFloat("speed", speed);
         animator.SetFloat("xInput", x);
         animator.SetFloat("yInput", y);
@@ -46,44 +46,56 @@ public class UnderwaterMovement : MonoBehaviour
         GameObject lamp = GameObject.FindGameObjectWithTag("DiverLamp");
         float xOffset = (float)Math.Cos(theta * Mathf.Deg2Rad);
         float yOffset = (float)Math.Sin(theta * Mathf.Deg2Rad);
+
         lamp.transform.position = new Vector3(x + xOffset, y + yOffset);
         lamp.transform.rotation = Quaternion.Euler(0, 0, theta);
     }
 
     void Update()
     {
-        bool dead = GameObject.FindWithTag("PlayerHitbox").GetComponent<OxygenManagement>().dead;
+        bool dead = GameObject.FindWithTag("Player").GetComponent<OxygenManagement>().dead;
 
         if (!dead)
         {
             // Get input for movement
             float moveX = Input.GetAxis("Horizontal");
             float moveY = Input.GetAxis("Vertical");
-            // Create a movement vector
-            Vector3 move = new Vector3(moveX, moveY, 0);
 
+            // Create a movement vector
+            Vector2 move = new Vector2(moveX, moveY);
+
+            // Normalize movement to prevent diagonal speed boost
             if (move.magnitude > 1)
             {
                 move.Normalize();
             }
 
-            controller.Move(move * speed * Time.deltaTime);
-            float movementSpeed = controller.velocity.magnitude;
-            Vector3 PlayerPOS = GameObject.FindGameObjectWithTag("Player").transform.position;
-            SetSpeedAndDirection(prevXNonZero, controller.velocity.x, controller.velocity.y, movementSpeed);
-            float direction = (float)Math.Atan(controller.velocity.y / controller.velocity.x) * 180.0f / (float)Math.PI;
-            direction = controller.velocity.x < 0 ? direction - 180 : direction;
+            // Apply movement using Rigidbody2D
+            rb2d.velocity = move * speed;
 
-            if (movementSpeed != 0)
+            // Calculate movement speed
+            float movementSpeed = rb2d.velocity.magnitude;
+
+            // Update direction and animation
+            SetSpeedAndDirection(prevXNonZero, rb2d.velocity.x, rb2d.velocity.y, movementSpeed);
+
+            // Update lamp position and rotation
+            Vector2 playerPos = rb2d.position;
+            float direction = Mathf.Atan2(rb2d.velocity.y, rb2d.velocity.x) * Mathf.Rad2Deg;
+
+            if (movementSpeed > 0)
             {
-                UpdateLampPose(PlayerPOS.x, PlayerPOS.y, direction);
+                UpdateLampPose(playerPos.x, playerPos.y, direction);
             }
-            prevXNonZero = moveX != 0? moveX : prevXNonZero;
+
+            // Keep track of the last non-zero horizontal movement
+            prevXNonZero = moveX != 0 ? moveX : prevXNonZero;
         }
         else
         {
+            // Stop all movement if the player is dead
+            rb2d.velocity = Vector2.zero;
             SetSpeedAndDirection(prevXNonZero, 0, 0, 0);
         }
-
     }
 }
